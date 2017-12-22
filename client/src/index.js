af1897bd94c6c4ebabb1294014bac10c721baa30
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-22 16:02:56 -0500

    initial setup and redux reducers setup

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './App';
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , root)
