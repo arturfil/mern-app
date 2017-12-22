51cfa50e32a78cbf1f58242ee1046cf43f396475
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-22 16:38:47 -0500

    added react-router and setup of other components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Landing from './components/Landing';
+
+const Header = () => <h2>Header</h2>
+const Dasboard = () => <h2>Dasboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h2>Testing</h2>
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" component={Landing} />
+                    </div>
+                </BrowserRouter>
             </div>
         )
     }
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h2>Landing Component</h2>
+        </div>
+    )
+}
+
+export default Landing;
