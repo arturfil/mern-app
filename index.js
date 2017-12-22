be5467734dfc7dfd81ffc3bb28463f03b1654f9d
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-21 22:37:52 -0500

    setting server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const passport = require('./services/passport-config');
+const authRouter = require('./routes/auth-router');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send({ text: 'test'});
-});
+authRouter(app);
+
+
+app.use(passport);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('starting MERN app server...')
 })
+
+module.exports  = authRouter;
