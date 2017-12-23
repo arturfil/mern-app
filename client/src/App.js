4d089ba11db646a14e70c91b4930e93f1d6d4296
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-23 18:53:06 -0500

    minor refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Aux from './hoc/Aux';
 import Landing from './components/Landing';
 import Header from './components/Header';
+import * as actions from './actions/actionTypes';
 
 import './App.css';
 
@@ -11,15 +13,22 @@ const Dasboard = () => <h2>Dasboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
     render() {
         return (
             <Aux>
                 <BrowserRouter>   
                     <Aux className="App">
                         <Header />
-                        <Route path="/" exact component={Landing} />
-                        <Route path="/surveys" exact component={Dasboard} />
-                        <Route path="/surveys/new" component={SurveyNew} />
+                        <div className="container">
+                            <Route path="/" exact component={Landing} />
+                            <Route path="/surveys" exact component={Dasboard} />
+                            <Route path="/surveys/new" component={SurveyNew} />
+                        </div>
                     </Aux>                 
                 </BrowserRouter>
             </Aux>
@@ -27,4 +36,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(null, actions)(App);
